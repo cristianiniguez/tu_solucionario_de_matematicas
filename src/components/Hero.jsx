4de219e515124439fe362f8f9e9d84ba5e60fd82
criniguez <felizpc1@gmail.com>
2020-10-18 19:30:04 -0400

    home: hero maden

---
+++ src/components/Hero.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../assets/styles/components/Hero.css';
+
+import pizarra from '../assets/images/pizarra.jpg';
+
+const Hero = () => {
+  const styles = {
+    backgroundImage: `linear-gradient(0deg, white 0%, transparent 10%), url(${pizarra})`,
+  };
+  return (
+    <section className='hero' style={styles}>
+      <div className='hero__container container'>
+        <h1 className='hero__title title'>
+          <div>
+            Bienvenido a <br />
+            <span className='text--red'>
+              tu
+            </span> <span className='text--blue'>solucionario</span> de{' '}
+            <span className='text--green'>matemáticas</span>
+          </div>
+        </h1>
+        <p className='hero__description'>
+          Cursos de matemáticas gratis para estudiantes escolares y universitarios
+        </p>
+        <div className='hero__buttons'>
+          <Link className='btn btn--red' to='/empezar'>
+            Empezar ya!
+          </Link>
+          <Link className='btn btn--green' to='/materias'>
+            Ver materias
+          </Link>
+          <Link className='btn btn--blue' to='/recursos'>
+            Ver recursos
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
