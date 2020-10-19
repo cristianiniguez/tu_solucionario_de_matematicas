4dcfa780ad0350b19cb53b1e8ba8726641e5dcfd
criniguez <felizpc1@gmail.com>
2020-10-19 18:57:40 -0400

    styles: media queries in nav-menu

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -4,30 +4,37 @@ import { Link } from 'react-router-dom';
 import '../assets/styles/components/NavMenu.css';
 
 const NavMenu = () => {
+  const [active, setActive] = React.useState(false);
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
-    <nav className='nav'>
+    <nav className={`nav ${active ? 'nav--active' : ''}`}>
       <ul className='nav-menu'>
         <li className='nav-menu__item'>
-          <Link className='nav-menu__link' to='/'>
+          <Link className='nav-menu__link' to='/' onClick={() => setActive(false)}>
             Inicio
           </Link>
         </li>
         <li className='nav-menu__item'>
-          <Link className='nav-menu__link' to='/materias'>
+          <Link className='nav-menu__link' to='/materias' onClick={() => setActive(false)}>
             Materias
           </Link>
         </li>
         <li className='nav-menu__item'>
-          <Link className='nav-menu__link' to='/recursos'>
+          <Link className='nav-menu__link' to='/recursos' onClick={() => setActive(false)}>
             Recursos
           </Link>
         </li>
         <li className='nav-menu__item'>
-          <Link className='nav-menu__link' to='/acerca'>
+          <Link className='nav-menu__link' to='/acerca' onClick={() => setActive(false)}>
             Acerca de
           </Link>
         </li>
       </ul>
+      <button onClick={handleClick} className='nav__btn'>
+        <i className='fas fa-bars'></i>
+      </button>
     </nav>
   );
 };
