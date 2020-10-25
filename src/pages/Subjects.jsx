58533a1b7dd5c82e1c91478c90bef77b93efe392
criniguez <felizpc1@gmail.com>
2020-10-25 17:49:05 -0400

    subjects-page: subjects page done

---
+++ src/pages/Subjects.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../assets/styles/pages/Subjects.css';
+
+import allSubjects from '../utils/subjects';
+
+export class Subjects extends React.Component {
+  render() {
+    return (
+      <main>
+        <section className='subjects-section'>
+          <div className='container'>
+            <h1 className='subjects-section__title title title-center'>Materias</h1>
+            <div className='subjects-section__grid'>
+              {Object.keys(allSubjects).map((sbj) => {
+                const { id, name, logo } = allSubjects[sbj];
+                return (
+                  <Link key={id} to={`/materias/${sbj}`} className='subjects-section__link'>
+                    <figure className='subjects-section__img'>
+                      <img src={logo} alt={name} />
+                    </figure>
+                    <h2 className='subjects-section__subtitle subtitle subtitle-center'>{name}</h2>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default Subjects;
--- src/router/App.jsx
+++ src/router/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Course from '../pages/Course';
+import Subjects from '../pages/Subjects';
 import NotFound from '../pages/NotFound';
 import Layout from '../components/Layout';
 
@@ -14,6 +15,7 @@ const App = () => (
     <Layout>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/materias' component={Subjects} />
         <Route exact path='/curso/:id' component={Course} />
         <Route component={NotFound} />
       </Switch>
