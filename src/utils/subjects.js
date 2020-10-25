eda946a11d688df89466bc1f34884f38a8edd364
criniguez <felizpc1@gmail.com>
2020-10-25 16:04:05 -0400

    assets: subjects logos and colors added

---
+++ src/utils/subjects.js
@@ -0,0 +1,35 @@
+import logo_aritmetica from '../assets/images/aritmetica.png';
+import logo_algebra from '../assets/images/algebra.png';
+import logo_geometria from '../assets/images/geometria.png';
+import logo_trigonometria from '../assets/images/trigonometria.png';
+import logo_calculo from '../assets/images/calculo.png';
+import logo_olimpiadas from '../assets/images/olimpiadas.png';
+
+const subjects = {
+  aritmetica: {
+    logo: logo_aritmetica,
+    color: 'var(--aritmetica-color)',
+  },
+  algebra: {
+    logo: logo_algebra,
+    color: 'var(--algebra-color)',
+  },
+  geometria: {
+    logo: logo_geometria,
+    color: 'var(--geometria-color)',
+  },
+  trigonometria: {
+    logo: logo_trigonometria,
+    color: 'var(--trigonometria-color)',
+  },
+  calculo: {
+    logo: logo_calculo,
+    color: 'var(--calculo-color)',
+  },
+  olimpiadas: {
+    logo: logo_olimpiadas,
+    color: 'var(--olimpiadas-color)',
+  },
+};
+
+export default subjects;
