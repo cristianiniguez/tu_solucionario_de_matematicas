15b775c534db601c8d64642e07da63713f79896c
criniguez <felizpc1@gmail.com>
2020-11-01 13:34:00 -0400

    footer: about links deleted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,26 +7,33 @@ const Footer = () => {
     <footer className='footer'>
       <div className='footer__container container'>
         <section className='footer__section social-links'>
-          <h2 className='title'>Redes sociales</h2>
-          <a href='https://www.facebook.com/tusolucionariodematematicas' target='_blank'>
-            <i className='fab fa-facebook' />
-            Facebook
-          </a>
-          <a href='https://www.youtube.com/channel/UCfndi4CyqsQc0zRzrMBgiOw' target='_blank'>
-            <i className='fab fa-youtube' />
-            Youtube
-          </a>
+          <p>
+            <a
+              className='footer__social-link'
+              href='https://www.facebook.com/tusolucionariodematematicas'
+              target='_blank'
+            >
+              <i className='fab fa-facebook' />
+              Facebook
+            </a>
+            <a
+              className='footer__social-link'
+              href='https://www.youtube.com/channel/UCfndi4CyqsQc0zRzrMBgiOw'
+              target='_blank'
+            >
+              <i className='fab fa-youtube' />
+              Youtube
+            </a>
+          </p>
         </section>
-        <section className='footer__section about'>
-          <h2 className='title'>Acerca de</h2>
-          <a href='#'>
-            <i className='fas fa-question-circle' />
-            ¿Qué es ... ?
-          </a>
-          <a href='#'>
-            <i className='fas fa-question-circle' />
-            ¿Quién es ... ?
-          </a>
+        <section className='footer__section copy'>
+          <p>
+            &copy;{' '}
+            <a href='https://cristianiniguez.github.io' target='_blank'>
+              Cristian iñiguez
+            </a>{' '}
+            - 2020
+          </p>
         </section>
       </div>
     </footer>
