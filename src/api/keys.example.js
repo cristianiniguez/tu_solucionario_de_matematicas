8c9bb659022b7695bffc4a68901a664f41ce31ad
criniguez <felizpc1@gmail.com>
2020-11-03 10:21:39 -0400

    keys: keys.example.js added

---
+++ src/api/keys.example.js
@@ -0,0 +1,5 @@
+const KEYS = {
+  YOUTUBE_API_KEY: '...',
+};
+
+export default KEYS;
--- src/api/keys.js
+++
@@ -1,5 +0,0 @@
-const KEYS = {
-  YOUTUBE_API_KEY: 'AIzaSyAbkf9kx_MpJGbQ0Bz5sYhylwAks4DBwbc',
-};
-
-export default KEYS;
